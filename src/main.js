12881a8bd48671b57ff565e418c42a493f45f115
vic <vicbruce@163.com>
2019-08-27 15:01:34 +0800

    the first save

--- src/main.js
+++ src/main.js
@@ -1,16 +1,28 @@
 //入口文件
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
-import app from './App.vue';
 
-import { Header } from 'mint-ui';
+import './lib/mui/css/mui.css';
+import './lib/mui/css/icons-extra.css';
+import './css/index.css'
 
-import './lib/mui/css/mui.css'
+import { Header,Swipe, SwipeItem } from 'mint-ui';
+
+
 
 Vue.component('bbb',Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+import router from './router.js';
+
+import app from './App.vue';
 
 
 var vm = new Vue({
   el:'#app',
   render:c=>c(app),
+  router
 })
--- src/router.js
+++ src/router.js
@@ -1,12 +1,22 @@
 import VueRouter from 'vue-router'
 
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShoppingContainer from './components/tabbar/ShoppingContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+
 
 
 
 var router = new VueRouter({
   routes: [
-    
-  ]
+    {path:'/',redirect:'/home'},
+    {path:'/home',component:HomeContainer},
+    {path:'/member',component:MemberContainer},
+    {path:'/shopping',component:ShoppingContainer},
+    {path:'/search',component:SearchContainer},
+  ],
+  linkActiveClass:'mui-active',
 })
 
 // 把路由对象暴露出去
